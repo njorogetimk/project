93a626835a1277694c21b71298dff12f87acc377
njorogetimk <njorogetimk@gmail.com>
2018-07-17 11:36:51 +0300

    Began Fuzzy analysis

--- myparameters_v2.m
+++ myparameters_v2.m
@@ -1,8 +1,29 @@
 %Parametes used in my project
-Fc=3780;                    % PWM Carrier frequency (Hz) 
-Ts_Power=1/Fc/200;          % SimPowerSystems sample time (s)
-Ts_Control=20*Ts_Power;     % Inverter Control system sample time (s)
+% Fc=3780;                    % PWM Carrier frequency (Hz) 
+% Ts_Power=1/Fc/200;          % SimPowerSystems sample time (s)
+% Ts_Control=20*Ts_Power;     % Inverter Control system sample time (s)
 fuzzy_controller = readfis('fuzzy_controller');
 
-pv_data = xlsread('power_v2_data.xlsx','B2:Y2');
+% RAW data
+time = xlsread('power_v2_data.xlsx','B1:Y1');
+G_data = xlsread('power_v2_data.xlsx','B2:Y2');
+T_data = xlsread('power_v2_data.xlsx','B3:Y3');
+load_data = xlsread('power_v2_data.xlsx','B4:Y4');
 ut_price = xlsread('power_v2_data.xlsx','B5:Y5');
+
+% Manufacturers data sheet data Solar panel
+Pstc = 120;
+Cp = 0.45;
+Tr = 25;
+Gstc = 1000;
+Istc = 6.6;
+Vstc = 18.1;
+
+% Battery parameters
+Q = 590; % Battery capacity
+SOC = 100; %Initial state of charge
+% Time vector
+% t = vct;
+% for i = 1:23
+%     t(i+1) = t(i)+1;
+% end
