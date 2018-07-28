a05c8a5fe8d843a71e89fca17b34f7b27f924334
njorogetimk <njorogetimk@gmail.com>
2018-07-28 16:52:09 +0300

    GUI for simulatin added

--- myparameters_v2.m
+++ myparameters_v2.m
@@ -1,7 +1,4 @@
-%Parametes used in my project
-% Fc=3780;                    % PWM Carrier frequency (Hz) 
-% Ts_Power=1/Fc/200;          % SimPowerSystems sample time (s)
-% Ts_Control=20*Ts_Power;     % Inverter Control system sample time (s)
+
 fuzzy_controller = readfis('fuzzy_controller');
 
 % RAW data
